72d3139b41fad23de6ab4aa4de41ef786c9b602b
EpicRaisin <gabe@muldowneyclan.com>
2024-05-13 20:07:14 -0500

    commnads

--- main.py
+++ main.py
@@ -4,9 +4,7 @@ import os
 from PIL import Image
 import re
 import modules.data
-
-owner = 663186731725488138
-subowners = [993927156142981240, 993927897477828790]
+from modules.commands import commands, groups
 
 os.system("clear")
 
@@ -19,42 +17,6 @@ bot = discord.Client(intents = intents)
 # testy = bot.get_guild(717048644708073534)
 
 afkMessages = []
-
-commands = {}
-groups = []
-
-class permissions:
-    @staticmethod
-    def is_owner(user : discord.Member):
-        return user.id == owner
-    
-    @staticmethod
-    def is_subowner(user : discord.Member):
-        return user.id == owner or user.id in subowners
-    
-    @staticmethod
-    def is_admin(user : discord.Member):
-        return user.guild_permissions.administrator
-
-def command(func):
-    print(f"Registered command {func.__name__}")
-    if func.__name__.lower() not in commands.keys():
-        commands[func.__name__.lower()] = {"function": func, "permissions": None}
-    def wrapper(msg, piped, args, **kwargs):
-        asyncio.run(func(msg, piped, args, **kwargs))
-    return wrapper
-
-def perms(func, *args):
-    commands[func.__name__.lower()] = {"function": func, "permissions": args}
-    return func
-
-def group(clas):
-    groups.append(clas.__name__.lower())
-    functions = [func for func in dir(clas) if not func.startswith("__")]
-    for func in functions:
-        real_func = getattr(clas, func)
-        commands[f"{clas.__name__.lower()}:{func.lower()}"] = real_func
-    print(f"Registered command group {clas.__name__.lower()} ({','.join(functions)})")
 
 @bot.event
 async def on_message(msg : discord.Message):
@@ -96,7 +58,7 @@ async def on_message(msg : discord.Message):
 
         if index == len(words) or word == "|":
             if cmd.lower() in groups:
-                cmd = f"{cmd}:{args[0].lower()}"
+                cmd = f"{cmd}.{args[0].lower()}"
                 args.pop(0)
             if not cmd in commands:
                 error = True
@@ -116,15 +78,15 @@ async def on_message(msg : discord.Message):
     if error:
         await msg.channel.send("That is not a command fool.")
 
-@bot.event
-async def on_reaction_add(reaction : discord.Reaction, user : discord.Member):
-    server = user.guild.id
-    server_check(server)
-    if reaction.emoji in servers[str(server)]["boards"].keys():
-        boar = servers[str(server)]["boards"][reaction.emoji]
-        if reaction.count >= boar["count"]:
-            if not reaction.message.id in boar["messages"]:
-                await add_to_board(boar, reaction.message, reaction.emoji)
+# @bot.event
+# async def on_reaction_add(reaction : discord.Reaction, user : discord.Member):
+#     server = user.guild.id
+#     server_check(server)
+#     if reaction.emoji in servers[str(server)]["boards"].keys():
+#         boar = servers[str(server)]["boards"][reaction.emoji]
+#         if reaction.count >= boar["count"]:
+#             if not reaction.message.id in boar["messages"]:
+#                 await add_to_board(boar, reaction.message, reaction.emoji)
 
 
 async def guild_emoji(guild):
